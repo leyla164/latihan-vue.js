9d9462d52e6c4020d84c0b1593e999b7b298f7d7
leyla164 <94092307+leyla164@users.noreply.github.com>
2023-07-25 14:53:08 +0700

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h3>{{ text }}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+   name: "Header",
+   props: ['text'],
+}
+</script>
+
+<style>
+   h3 {
+     color: black;
+     background-color: aquamarine;
+     text-align: center;
+   }
+ 
+</style>
